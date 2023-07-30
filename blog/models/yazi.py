903749584216f511b2681a206c03327e31af9096
Kemal Özcan <kemalozccan@gmail.com>
2023-07-30 23:43:43 +0300

    yazilar modeli admin paneline eklendi. ckeditor yüklendi.

--- blog/models/yazi.py
+++ blog/models/yazi.py
@@ -2,13 +2,14 @@ from django.db import models
 from autoslug import AutoSlugField
 from blog.models import KategoriModel
 from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
 
 class YazilarModel(models.Model):
     resim = models.ImageField(upload_to='yazi_resimleri')
     baslik = models.CharField(max_length=50)
-    icerik = models.TextField()
+    icerik = RichTextField()
     olusturulma_tarihi = models.DateTimeField(auto_now_add=True)
-    duzenleneme_tarihi = models.DateTimeField(auto_now=True)
+    duzenlenme_tarihi = models.DateTimeField(auto_now=True)
     slug = AutoSlugField(populate_from = 'baslik', unique=True)
     kategoriler = models.ManyToManyField(KategoriModel, related_name='yazi')
     yazar = models.ForeignKey(User, on_delete=models.CASCADE, related_name='yazilar')
@@ -17,3 +18,6 @@ class YazilarModel(models.Model):
         verbose_name = 'Yazi'
         verbose_name_plural = 'Yazilar'
         db_table = 'Yazi'
+
+    def __str__(self):
+        return self.baslik
