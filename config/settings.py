a266cc41c171eba7bd69a7f8b8265b3d9db95d65
Kemal Özcan <kemalozccan@gmail.com>
2023-08-06 23:32:01 +0300

    user modele avatar alanı eklendi.

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 2. taraf uygulamalar
     'blog',
+    'account',
+    # 3. taraf uygulamalar
     'ckeditor',
 ]
 
@@ -123,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'account.CustomUserModel'
